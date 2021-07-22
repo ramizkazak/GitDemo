6639e2b618f5fc4e27424446d6ccebd43b8d28e9
ramiz <ramizkazak@gmail.com>
2021-07-21 20:58:37 -0500

    this is commit from Asian Timezone guy

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,14 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
+
+}
