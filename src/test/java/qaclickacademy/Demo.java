8a4a000bef56bc3e4e69cf9da1cd91e787f3e5d5
ramiz <ramizkazak@gmail.com>
2021-07-22 22:10:35 -0500

    Assian development Add Final codes in Demo Class

--- src/test/java/qaclickacademy/Demo.java
+++ src/test/java/qaclickacademy/Demo.java
@@ -1,5 +1,22 @@
 package qaclickacademy;
 
+import org.junit.Test;
+
 public class Demo {
+	
+	
+	
+	@Test 
+	public void demostration1() {
+		System.out.println("demostration1 codes");
+	}
+	@Test 
+	public void demostration2() {
+		System.out.println("demostration2 codes");
+	}
+	@Test 
+	public void demostration3() {
+		System.out.println("demostration3 codes");
+	}
 
 }
