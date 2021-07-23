f6374bd86b588302f1ca3ccfb7fe6faf2cf38a40
ramiz <ramizkazak@gmail.com>
2021-07-22 22:15:49 -0500

    New Verification class creatge from American development

---
+++ src/test/java/qaclickacademy/Verification.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class Verification {
+	
+	@Test 
+	public void verification1() {
+		System.out.println("verification1 codes");
+	}
+	@Test 
+	public void verification2() {
+		System.out.println("verification2 codes");
+	}
+	@Test 
+	public void verification3() {
+		System.out.println("verification3 codes");
+	}
+
+}
